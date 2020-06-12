c844be714cf19e31a29389a004cee66782857e78
Nelson_Olanipekun <olanipekunnelson@gmail.com>
2020-06-12 16:24:41 +0100

    This is datastructure in dart class

---
+++ datastructure_in_darts/datastructures_in_darts/bin/main.dart
@@ -0,0 +1,21 @@
+import 'package:datastructures_in_darts/datastructures_in_darts.dart' as datastructures_in_darts;
+
+void main(List<String> arguments) {
+  // print('${datastructures_in_darts.printFruits()}!');
+  // List myList = [];
+  // myList.add(45);
+  // myList.add(50);
+  // myList.add(77);
+  // print(myList);
+
+
+  // print('${datastructures_in_darts.getSum(3)}');
+  List mySumList = datastructures_in_darts.getSumList(3);
+  int mySum = mySumList.reduce((value, element) => value + element);
+  print(mySum);
+
+}
+
+
+// 1. Write a function to add an item into an existing List => (the function return a List and accepts a string and an existing List)
+// 2. Write a function to return the position of an item in a List => (the function return an int and accepts a string and an existing List
---
+++ datastructure_in_darts/datastructures_in_darts/lib/datastructures_in_darts.dart
@@ -0,0 +1,25 @@
+List printFruits() {
+  var food = ["apples", "mangoes", "oranges", "paw-paw"];
+
+  return food;
+}
+
+int getSum ( int n) {
+  int sum = 0;
+  for ( int i = 0; i <= n ; i++){
+    print(i);
+    sum = sum + i;
+  }
+
+  print(n);
+}
+
+List getSumList( int n) {
+  List myList  = [ ];
+
+  for ( int i = 0 ; i <= n; i++){
+    myList.add(i);
+  }
+
+  return myList;
+}
