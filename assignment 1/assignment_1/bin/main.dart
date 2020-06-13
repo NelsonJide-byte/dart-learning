490754f9b9dcdb1373be17a5348dc5729873e084
Nelson_Olanipekun <olanipekunnelson@gmail.com>
2020-06-13 15:55:16 +0100

    my 2nd  assignment

--- assignment 1/assignment_1/bin/main.dart
+++ assignment 1/assignment_1/bin/main.dart
@@ -1,13 +1,13 @@
 import 'package:assignment_1/assignment_1.dart' as assignment_1;
-void main() {
-    print('hello ${i + 1}');
+
+void main () {
+  
+  //List myFruits = ["apples", "oranges", "mangoes"];
+
+  //print(assignment_1.myPlacement(myFruits, 6));
+
+List car = ["camry", "corolla", "tesla"];
+print(assignment_1.myRow(car, 2018));
+
 }
 
-
-
-
-
-//void main(){
-
- //mylist();
-//}
--- assignment 1/assignment_1/lib/assignment_1.dart
+++ assignment 1/assignment_1/lib/assignment_1.dart
@@ -1,17 +1,19 @@
-for (int i = 0; i < 5; i++) {
-  
+List even_list (){
+    List mylist = [1,2,3,4,5,6,7];
+    return mylist;
   }
 
+var play = "I want to go to the market";
+var myFruits = ["apples", "mangoes", "bananas"];
+    // myFruits.insertlist = (["orange"]);
+    // print(myFruits); 
 
+List myPlacement(List originalList, var n){
+  originalList.add(n);
+  return originalList;
+}
 
-
-
-
-
-
-
-
-//void even_list (){
- //   void mylist = [1,2,3,4,5,6,7];
-  //  print(mylist);
-  }
+List myRow(List car, var p,){
+  car.add(p);
+  return car;
+}
