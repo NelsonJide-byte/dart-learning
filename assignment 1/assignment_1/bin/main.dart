369a59e5ff36ca3419ca14773cc85c6412cf1089
Nelson_Olanipekun <olanipekunnelson@gmail.com>
2020-06-13 13:14:04 +0100

    my new assignment

---
+++ assignment 1/assignment_1/bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:assignment_1/assignment_1.dart' as assignment_1;
+void main() {
+    print('hello ${i + 1}');
+}
+
+
+
+
+
+//void main(){
+
+ //mylist();
+//}
---
+++ assignment 1/assignment_1/lib/assignment_1.dart
@@ -0,0 +1,17 @@
+for (int i = 0; i < 5; i++) {
+  
+  }
+
+
+
+
+
+
+
+
+
+
+//void even_list (){
+ //   void mylist = [1,2,3,4,5,6,7];
+  //  print(mylist);
+  }
